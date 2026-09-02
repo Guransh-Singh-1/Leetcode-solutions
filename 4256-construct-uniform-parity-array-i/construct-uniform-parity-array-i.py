7e9df70e6b8b10f511f38c1245eb42b84a5998d7
Guransh Singh <guransh059@gmail.com>
2026-09-02 17:48:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (47.47%) - LeetSync

---
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        return True
+
+    # answer always True
+    # case 1 = all even then its true
+    # case 2 = all odd then its true 
+    # case 3 = if mixed then again everytime true
+    # beacuse even - odd = odd
+    # and odd - even = odd
